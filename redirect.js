b411d62cf748e2a05bca4d483937ee7358b4db75
kyawhtet-github <152606140+kyawhtet-github@users.noreply.github.com>
2025-02-24 15:12:02 +0630

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,8 +2,9 @@ window.onload = function () {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
     var appStoreUrl = "https://apps.apple.com/us/app/supermyan/id6523422437";
     var playStoreUrl = "https://play.google.com/store/apps/details?id=com.uabfintech.supermyan&hl=en";
-    var fallbackUrl = "https://uatmoe.uabpay.com.mm/home";
+    var fallbackUrl = "https://drive.google.com/file/d/1FcHrU4vygSl4hmzfWHS-hrGIBSkwDT9L/view?usp=sharing";
     var buttonsDiv = document.getElementById("buttons");
+    var headerDiv = document.getElementById("header");
 
     function createImageButton(imageSrc, url, altText, text) {
         var link = document.createElement("a");
@@ -16,6 +17,16 @@ window.onload = function () {
         `;
         buttonsDiv.appendChild(link);
     }
+
+    function createLogoHeader(imageSrc, altText) {
+        var logo = document.createElement("img");
+        logo.src = imageSrc;
+        logo.alt = altText;
+        logo.className = "w-24 mx-auto mb-4";
+        headerDiv.appendChild(logo);
+    }
+
+    createLogoHeader("images/supermyan-logo.png", "Supermyan Logo");
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         createImageButton("images/appstore.png", appStoreUrl, "Download on the App Store", "Download on the App Store");
